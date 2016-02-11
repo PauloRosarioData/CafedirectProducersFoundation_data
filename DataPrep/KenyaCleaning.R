80e367fefef6ae84e3f1b65bd5662ccaa24ea657
Kate Vang <katevang@Macintosh-2.home>
2016-02-11 16:58:30 +0000

    Adding to dictionary and modifying R script through Q101

--- DataPrep/KenyaCleaning.R
+++ DataPrep/KenyaCleaning.R
@@ -1,6 +1,5 @@
-# Load the headers from the data file
-#OriginalColName <- read.csv("Addressing the -1 for DataKind - KENYA SURVEY - KMM UPDATES 22 JAN-1.csv",
-#                            header=FALSE, skip=1, nrows=1)
+#Note: first set working directory to the cloned Github repo
+
 # Load the mapping table of new to old column names and make into a vector
 ColumnNameData <- read.csv("NewColumnNameMapping.csv")
 ColumnNameMap <- ColumnNameData$NewColName
@@ -10,9 +9,9 @@ ColumnNameMap <- make.names(ColumnNameMap, unique=TRUE)
 #Load in data using new column names
 Kenya <- read.csv("Addressing the -1 for DataKind - KENYA SURVEY - KMM UPDATES 22 JAN cleaned data - Sheet1.csv",
                  header = FALSE, stringsAsFactors = FALSE, skip = 2, col.names = ColumnNameMap)
-#NewNames1314 <- Headings$Proposed.New.Name
-#names(NewNames1314) <- Headings$R.Version.1314
-#HSCIC1314 <- rename(HSCIC1314, NewNames1314)
+#Remove column name dataframe
+rm(ColumnNameData)
+rm(ColumnNameMap)
                             
 #Define function to make values into proper case
 properCase <- function(x) {
@@ -26,13 +25,25 @@ properCase <- function(x) {
 Kenya$Q3.CoopSociety <- sapply(Kenya$Q3.CoopSociety, properCase)
 Kenya$Q4.Village <- sapply(Kenya$Q4.Village, properCase)
 Kenya$Q7.ProducerOrg <- sapply(Kenya$Q7.ProducerOrg, properCase)
-                            
+                
+#For free text 'earnings' and 'cost' fields, extract only numeric digits
+#and convert them to integers or numbers
 library(stringr)
-#Extract only numeric digits of three fields and convert them to integers or numbers
 Kenya$Q38.CashCropEarnings <- as.integer(gsub("[^0-9]", "", Kenya$Q38.CashCropEarnings))
-#Kenya$Q39.CashCropPx <- as.numeric(gsub("[^0-9.]", "", Kenya$Q39.CashCropPx))
 Kenya$Q39.CashCropPx <- as.numeric(str_extract(Kenya$Q39.CashCropPx, "\\d{1,}\\.?(\\d{1,})?"))
 Kenya$Q40.CostOfProduction <- as.integer(gsub("[^0-9]", "", Kenya$Q40.CostOfProduction))
 Kenya$Q61.CropEarning <- as.integer(gsub("[^0-9]", "", Kenya$Q61.CropEarning))
 Kenya$Q63.ProductionCost <- as.integer(gsub("[^0-9]", "", Kenya$Q63.ProductionCost))
-
+Kenya$Q69.AmountEarnedCattle <- as.integer(gsub("[^0-9]", "", Kenya$Q69.AmountEarnedCattle))
+#Same here, but first replacing 'None' with 0. To-do: make this cleaner with regex!
+Kenya$Q70.NumberCattleSold <- gsub("None", 0, Kenya$Q70.NumberCattleSold)
+Kenya$Q70.NumberCattleSold <- gsub("none", 0, Kenya$Q70.NumberCattleSold)
+Kenya$Q70.NumberCattleSold <- gsub("NONE", 0, Kenya$Q70.NumberCattleSold)
+Kenya$Q70.NumberCattleSold <- as.integer(gsub("[^0-9]", "", Kenya$Q70.NumberCattleSold))
+Kenya$Q71.PxPerCattle <- as.integer(gsub("[^0-9]", "", Kenya$Q71.PxPerCattle))
+Kenya$Q72.TotalEarningCattle <- as.integer(gsub("[^0-9]", "", Kenya$Q72.TotalEarningCattle))
+Kenya$Q81.AmountEarnedChickens <- as.integer(gsub("[^0-9]", "", Kenya$Q81.AmountEarnedChickens))
+#Same here, but first replacing 'None' with 0. To-do: make this cleaner with regex!
+Kenya$Q82.NumberChickensSold <- gsub("None", 0, Kenya$Q82.NumberChickensSold)
+Kenya$Q82.NumberChickensSold <- gsub("none", 0, Kenya$Q82.NumberChickensSold)
+Kenya$Q82.NumberChickensSold <- as.integer(gsub("[^0-9]", "", Kenya$Q82.NumberChickensSold))
